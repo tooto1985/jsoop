af22662edb2504d91792c61dfc4d66cdea4f2b01
Tommy <tooto1985@gmail.com>
2016-06-22 17:03:52 +0800

    dev 5

--- complete/5-4.js
+++ complete/5-4.js
@@ -1,11 +1,11 @@
-//傳址參考
+//共享參考
 var arr1 = [1, 2, 3];
 var arr2 = arr1;
 arr2.push(4);
 console.log(arr1); //預期 [1,2,3]   實際 [1,2,3,4]
 console.log(arr2); //預期 [1,2,3,4] 實際 [1,2,3,4]
 
-//避免傳址參考
+//避免共享參考
 var arr1 = [1, 2, 3];
 var arr2 = arr1.slice(0); //slice會產生一個新的陣列
 arr2.push(4);
