9dc0f32ef066223a43c386856c966834b8980a32
Tommy <tooto1985@gmail.com>
2016-09-22 17:42:30 +0800

    test 3

---
+++ complete/6-9-2.js
@@ -0,0 +1,54 @@
+function Player(name) {
+    this.points = 0;
+    this.name = name;
+}
+Player.prototype.play = function() {
+    this.points += 1;
+    mediator.played();
+};
+var scoreboard = {
+    element: document.body,
+    update: function(score) {
+        var i, msg = "";
+        for (i in score) {
+            if (score.hasOwnProperty(i)) {
+                msg += "<p><strong>" + i + "<\/strong>:";
+                msg += score[i];
+                msg += "<\/p>";
+            }
+        }
+        this.element.innerHTML = msg;
+    }
+};
+var mediator = {
+    Players: {},
+    add: function(key, obj) {
+        this.Players[obj.name] = {
+            key: key,
+            obj: obj
+        };
+    },
+    played: function() {
+        var score = {};
+        for (var player in this.Players) {
+            score[player] = this.Players[player].obj.points;
+        }
+        scoreboard.update(score);
+    },
+    keypress: function(e) {
+        e = e || window.event;
+        for (var player in mediator.Players) {
+            player = mediator.Players[player];
+            if (e.which === player.key) {
+                player.obj.play();
+                return;
+            }
+        }
+    }
+};
+mediator.add(48, new Player("Home"));
+mediator.add(49, new Player("Guest"));
+mediator.add(50, new Player("Other"));
+mediator.add(51, new Player("Mark"));
+mediator.add(52, new Player("John"));
+window.onkeypress = mediator.keypress;
