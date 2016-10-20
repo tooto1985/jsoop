867f2fa9c8c0c1f0a63bec5b27397b8dc873d584
Tommy <tooto1985@gmail.com>
2016-10-20 15:14:22 +0800

    test 9

--- complete/2-7.js
+++ complete/2-7.js
@@ -1,9 +1,9 @@
 var add = (function() {
-	var count=0;
-	return function() {
-		return count += 1;
-	};
+    var count=0;
+    return function() {
+        return count += 1;
+    };
 })();
-console.log(add());
-console.log(add());
-console.log(add());
+console.log(add()); //1
+console.log(add()); //2
+console.log(add()); //3
