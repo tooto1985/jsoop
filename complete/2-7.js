ed895254255a8ae7a6bcd33412cbdeb173168bdb
Tommy <tooto1985@gmail.com>
2016-10-20 13:50:16 +0800

    test 8

--- complete/2-7.js
+++ complete/2-7.js
@@ -1,11 +1,9 @@
-function f(x) {
-    var temp = x;
-    return function(x) {
-        temp += x;
-        console.log(temp);
-    };
-}
-var a = f(50);
-a(5); //55
-a(10); //65
-a(20); //85
+var add = (function() {
+	var count=0;
+	return function() {
+		return count += 1;
+	};
+})();
+console.log(add());
+console.log(add());
+console.log(add());
