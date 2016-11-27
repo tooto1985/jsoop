36a8f6fde25a60b346d715e265fea425b066da24
tommy <tooto1985@gmail.com>
2016-11-27 10:59:49 +0800

    dev 23

--- complete/5-2.js
+++ complete/5-2.js
@@ -1,6 +1,6 @@
 function base() {
     console.log(this, arguments);
 }
-base.call(1, 2, 3); //第一個參數修改this,之後的參數為function參數
-base.apply(1, [2, 3]); //第一個參數修改this,第二個參數必須是陣列為function參數
-base.bind(1)(2, 3); //第一個參數修改this,回傳一個新的function
+base.call({name:"John"}, 2, 3); //第一個參數修改this,之後的參數為function參數
+base.apply({name:"Mark"}, [2, 3]); //第一個參數修改this,第二個參數必須是陣列為function參數
+base.bind({name:"Jack"})(2, 3); //第一個參數修改this,回傳一個新的function
