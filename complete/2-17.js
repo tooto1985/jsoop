5dafaa925107fcd27ff9412373b20776027ce304
Tommy <tooto1985@gmail.com>
2017-02-15 08:39:54 +0800

    dev 31

--- complete/2-17.js
+++ complete/2-17.js
@@ -1,8 +1,1 @@
-var a = 10;
-console.log(a.toString(2));
-console.log((5).toString(2));
-console.log(2 .toString(2));
-console.log(1..toString(2));
-[0.1, 0.125, 0.2, 0.25, 0.3, 0.4, 0.5].forEach(function(value) {
-    console.log(value + "=>" + value.toString(2));
-});
+console.log(0.1 + 0.2 === 0.3); //false
