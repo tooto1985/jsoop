fd39ed6681a65cafc57c619df4e456ca05f428a1
tommy <tooto1985@gmail.com>
2017-02-24 22:45:37 +0800

    test 17

--- complete/5-11.js
+++ complete/5-11.js
@@ -35,11 +35,7 @@ RangePoint.prototype.write = function(x,y) {
     x = x > this.max ? this.max : x;
     y = y < this.min ? this.min : y;
     y = y > this.max ? this.max : y;
-    if (this.constructor === RangePoint) {
-        this.constructor.uber.write.apply(this,arguments);
-    } else if (this.constructor === IntegerRangePoint) {
-        this.constructor.uber.constructor.uber.write.apply(this,arguments);
-    }
+    RangePoint.prototype.constructor.uber.write.apply(this,arguments);
 };
 var rp = new RangePoint(-50,50);
 rp.write(-100,100);
@@ -53,7 +49,7 @@ IntegerRangePoint.inherits(RangePoint);
 IntegerRangePoint.prototype.write = function(x,y) {
     x = Math.round(x);
     y = Math.round(y);
-    this.constructor.uber.write.apply(this,arguments);
+    IntegerRangePoint.prototype.constructor.uber.write.apply(this,arguments);
 };
 var irp = new IntegerRangePoint(0,10);
 irp.write(9.1,7.8);
