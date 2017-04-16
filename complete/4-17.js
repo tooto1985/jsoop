98b8368b6d324a3a6d082b422035bd40c6acda29
tommy <tooto1985@gmail.com>
2017-04-16 11:27:11 +0800

    dev 35

--- complete/4-17.js
+++ complete/4-17.js
@@ -1,6 +1,6 @@
 var o = new Object(1);
-console.log(o.constructor === Number); //true (不應該有Number的原型方法)
-var o = new Object("harry");
-console.log(o.constructor === String); //true (不應該有String的原型方法)
+console.log(o.constructor); //function Number() (不應該有Number的原型方法)
+var o = new Object("text");
+console.log(o.constructor); //function String() (不應該有String的原型方法)
 var o = new Object(true);
-console.log(o.constructor === Boolean); //true(不應該有Boolean的原型方法)
+console.log(o.constructor); //function Boolean() (不應該有Boolean的原型方法)
