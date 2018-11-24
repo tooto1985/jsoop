4ce1b4a33548c1e75ba0f50e296dce9904d4cc4a
tommy <tooto1985@gmail.com>
2018-11-25 02:48:49 +0800

    test 27

--- complete/7-1.js
+++ complete/7-1.js
@@ -16,3 +16,23 @@
     }
     console.log(a);
 }
+//let在for迴圈中
+for (let i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }, i * 1000);
+}
+//let看起來好像沒有hoisting
+{
+    console.log(a);
+    let a = 1;
+}
+//let其實有hoisting但無法操作(又稱作死區Dead Zone)
+{
+    let a = 2;
+    {
+        console.log(a);
+        let a = 1;
+    }
+}
+
