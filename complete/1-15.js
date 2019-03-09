1f5e35bf6101183521e2a4b53fef978631959dae
tommy <tooto1985@gmail.com>
2019-03-09 21:21:51 +0800

    test 32

--- complete/1-15.js
+++ complete/1-15.js
@@ -1,11 +1,11 @@
 var n = 1;
-if (n == 1 || n == 2 || n == 3) {
+/* if (n == 1 || n == 2 || n == 3) {
     console.log("n<=3");
 } else if (n == 4 || n == 5) {
     console.log("n>3 & n<=5");
 } else {
     console.log("n>5");
-}
+} */
 switch (n) {
     case 1:
     case 2:
---
+++ exercise/1-15.js
@@ -0,0 +1,8 @@
+var n = 1;
+/* if (n == 1 || n == 2 || n == 3) {
+    console.log("n<=3");
+} else if (n == 4 || n == 5) {
+    console.log("n>3 & n<=5");
+} else {
+    console.log("n>5");
+} */
